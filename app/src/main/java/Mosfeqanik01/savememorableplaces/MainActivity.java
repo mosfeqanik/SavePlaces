1d0251175f64b7d150b985b79f060cc146ff489b
mosfeqanik <mosfeqanik01@gmail.com>
2020-01-13 13:34:25 +0600

    First commit

---
+++ app/src/main/java/Mosfeqanik01/savememorableplaces/MainActivity.java
@@ -0,0 +1,46 @@
+package Mosfeqanik01.savememorableplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> places = new ArrayList<String>();
+    static ArrayList<LatLng> locations = new ArrayList<LatLng>();
+    static ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        places.add(" Add More Places....");
+        locations.add(new LatLng(0,0));
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("places",position);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
